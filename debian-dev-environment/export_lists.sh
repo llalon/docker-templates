cfacfabade0104ff5f9946a1a5168650ffd1be7f
llalon <71791017+llalon@users.noreply.github.com>
2021-07-16 16:55:29 -0400

    User

--- debian-dev-environment/export_lists.sh
+++ debian-dev-environment/export_lists.sh
@@ -3,9 +3,11 @@
 # Run this script on the host to generate package lists and source keys for the docker image
 
 mkdir -p ./apt/sources/
-mkdir -p ./apt/keys
+mkdir -p ./apt/keyrings
 
 dpkg --get-selections > ./apt/packages.list
-cp -R /etc/apt/sources.list* ./apt/sources
+cp -rv /etc/apt/sources.list* ./apt/sources
 apt-key exportall > ./apt/repo.keys
-cp -R /usr/share/keyrings ./apt/keys
+cp -rv /usr/share/keyrings/* ./apt/keyrings/
+
+debconf-get-selections > ./apt/debconf-selections
